067f108903fd6f007434eb0b8118787af2d084af
Ye Zhang <mr.yezhang@gmail.com>
2022-07-23 22:15:36 -0700

    Add logic to iterate each file under a folder.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import glob
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def process_file(file_name):
+    print(file_name)
+
+
+# Folder to be processed. See https://docs.python.org/3/library/glob.html for format.
+folder_path = "/home/ye/Pictures/2018/2018_09_08/*"
+
+
+def process_folder(folder_path_pattern):
+    total_file = 0
+
+    for file in glob.iglob(folder_path_pattern, recursive=False):
+        total_file += 1
+        process_file(file)
+
+    print(f"Total file {total_file}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    process_folder(folder_path)
